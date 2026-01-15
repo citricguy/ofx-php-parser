d56c0a5471291a44d70fec5bb8bd724c4d5c6e29
Josh Sommers <citricguy@gmail.com>
2026-01-15 11:52:17 -1000

    Fixed stan issues.

--- tests/Unit/OfxDateTest.php
+++ tests/Unit/OfxDateTest.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Endeken\OFX\OFX;
+use DateTime;
 
 it('parses a simple date without offset', function (): void {
     $ref = new ReflectionMethod(OFX::class, 'parseDate');
     $ref->setAccessible(true);
 
     $dt = $ref->invoke(null, '20230101120000');
+    /** @var DateTime $dt */
 
     expect($dt)->toBeInstanceOf(DateTime::class);
     expect($dt->format('Y-m-d H:i:s'))->toBe('2023-01-01 12:00:00');
@@ -19,6 +21,7 @@ it('parses a date with numeric offset in brackets', function (): void {
     $ref->setAccessible(true);
 
     $dt = $ref->invoke(null, '20230101120000[-8]');
+    /** @var DateTime $dt */
 
     expect($dt)->toBeInstanceOf(DateTime::class);
     // offset in seconds is -8 hours
